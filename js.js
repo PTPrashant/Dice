1cb6123b15e9d81a20d9472e5fecc08237175189
PTPrashant <86474829+PTPrashant@users.noreply.github.com>
2021-12-24 12:27:58 +0530

    Add files via upload
    Added EventListener and removed on click ID.
    Now working with real-time results

--- js.js
+++ js.js
@@ -1,28 +1,36 @@
 var RandomNumber1;
 var RandomNumber2;
-var digit1 = 0;
-var digit2 = 0;
 var finaldigit;
 
 var Answer;
 
+document.querySelectorAll('button')[0].addEventListener('click',RunPlayer1);
+document.querySelectorAll('button')[1].addEventListener('click',RunPlayer2);
+
+
 function RunPlayer1() {
   RandomNumber1 = Math.floor(Math.random() * 6) + 1;
   document.querySelector('#dice1').setAttribute("src", "dice" + RandomNumber1 + ".png");
-  digit1 = 1;
+
+  if (RandomNumber1 > RandomNumber2) {
+
+    document.getElementById('Result').innerHTML = 'Are  BC Player 1 jeet gya bhaiii!!!!';
+  }
+
+
+  if (RandomNumber1 < RandomNumber2) {
+    document.getElementById('Result').innerHTML = 'Bhaukaal hai bhai player 2 ka bhaukaal hai!!';
+  }
+
+  if (RandomNumber1 == RandomNumber2)
+    document.getElementById('Result').innerHTML = 'Its a Draw !!';
 
 }
 
 function RunPlayer2() {
   RandomNumber2 = Math.floor(Math.random() * 6) + 1;
   document.querySelector('#dice2').setAttribute("src", "dice" + RandomNumber2 + ".png");
-  digit2 = 1;
 
-  finaldigit = digit1 + digit2;
-
-
-
-  if (finaldigit == 2) {
 
     if (RandomNumber1 > RandomNumber2) {
 
@@ -37,6 +45,6 @@ function RunPlayer2() {
     if (RandomNumber1 == RandomNumber2)
       document.getElementById('Result').innerHTML = 'Its a Draw !!';
 
-  }
+
 
 }
